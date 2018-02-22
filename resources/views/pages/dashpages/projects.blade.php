40f0255c5b19cf542b075da6ec67be8f15b55c12
edFortune <fortune.edmael1@gmail.com>
2018-02-22 15:48:11 -0500

    Add project modal items

--- resources/views/pages/dashpages/projects.blade.php
+++ resources/views/pages/dashpages/projects.blade.php
@@ -1,17 +1,15 @@
-@extends('layouts.dashboard',['title' => 'Project'])
-
-@section('content')
+@extends('layouts.dashboard',['title' => 'Project']) @section('content')
 <div class="container-fluid" data-ng-controller="ProjectsCtrl">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header" data-background-color="purple">
-                        <button type="button" class="btn btn-simple btn-md edit-profile-icon" data-toggle="modal" data-target="#addProjectModal">
-                                <i class="material-icons">add</i>
-                            </button>
+                    <button type="button" class="btn btn-simple btn-md edit-profile-icon" data-toggle="modal" data-target="#addProjectModal">
+                        <i class="material-icons">add</i>
+                    </button>
                     <h4 class="title">
                         Projects List
-                        
+
                     </h4>
                     <p class="category">Here is a subtitle for this table</p>
                 </div>
@@ -65,26 +63,35 @@
                 </div>
             </div>
         </div>
-        
+
     </div>
 
     <!-- Add Project Modal -->
     <div class="modal fade" id="addProjectModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add Project</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
-            </div>
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Add Project</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Title:</label>
+                            <input type="text" class="form-control" id="recipient-name">
+                        </div>
+                        <div class="form-group">
+                            <label for="message-text" class="col-form-label">Description:</label>
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
             </div>
         </div>
     </div>
