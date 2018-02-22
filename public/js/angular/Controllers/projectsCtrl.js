eae8469e16ecdf98e5fa212c8b5e4f3b1015f3c4
edFortune <fortune.edmael1@gmail.com>
2018-02-22 15:01:04 -0500

    Creating project modal

---
+++ public/js/angular/Controllers/projectsCtrl.js
@@ -0,0 +1,8 @@
+function ProjectsCtrl($scope) {
+
+    $scope.showAddProjectModal = function(){
+        console.log("Hey");
+        alert("HEY");
+    }
+
+}
--- resources/views/pages/dashpages/projects.blade.php
+++ resources/views/pages/dashpages/projects.blade.php
@@ -1,12 +1,18 @@
 @extends('layouts.dashboard',['title' => 'Project'])
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid" data-ng-controller="ProjectsCtrl">
     <div class="row">
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header" data-background-color="purple">
-                    <h4 class="title">Projects List</h4>
+                        <button type="button" class="btn btn-simple btn-md edit-profile-icon" data-ng-click="showAddProjectModal()">
+                                <i class="material-icons">add</i>
+                            </button>
+                    <h4 class="title">
+                        Projects List
+                        
+                    </h4>
                     <p class="category">Here is a subtitle for this table</p>
                 </div>
                 <div class="card-content table-responsive">
