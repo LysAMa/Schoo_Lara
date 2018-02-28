2e438e5e483c767e9620fa8421aa5a6a04eec1b3
edFortune <fortune.edmael1@gmail.com>
2018-02-28 09:06:34 -0500

    Profile Merge

--- resources/views/pages/dashpages/profile.blade.php
+++ resources/views/pages/dashpages/profile.blade.php
@@ -20,11 +20,7 @@
                 </div>
 
                 <div class="card-content">
-<<<<<<< HEAD
                     <form method="POST" action="">
-=======
-                    <form method="POST" action="{{ url('/profile/'.Auth::user()->id) }}">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
 
                         {{ csrf_field() }} {{ method_field('PUT')}}
                         <div class="row">
@@ -32,11 +28,7 @@
                                 <div class="form-group label-floating">
                                     <label class="control-label">Company</label>
 
-<<<<<<< HEAD
                                     <input type="text" name="orgName" class="form-control" data-ng-disabled="editProfile == false" value="{{ $profile->org_name}}">
-=======
-                                    <input type="text" name="orgName" class="form-control" data-ng-disabled="editProfile == false" value="">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                             <div class="col-md-3">
@@ -57,21 +49,13 @@
                             <div class="col-md-6">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Fist Name</label>
-<<<<<<< HEAD
                                     <input type="text" name="firstName" class="form-control" value="{{ $profile->first_name}}" data-ng-disabled="editProfile == false">
-=======
-                                    <input type="text" name="firstName" class="form-control" value="" data-ng-disabled="editProfile == false">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Last Name</label>
-<<<<<<< HEAD
                                     <input type="text" name="lastName" class="form-control" data-ng-disabled="editProfile == false" value="{{ $profile->last_name}}">
-=======
-                                    <input type="text" name="lastName" class="form-control" data-ng-disabled="editProfile == false" value="">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                         </div>
@@ -79,11 +63,7 @@
                             <div class="col-md-12">
                                 <div class="form-group label-floating">
                                     <label class="control-label">Address</label>
-<<<<<<< HEAD
                                     <input type="text" name="address" class="form-control" data-ng-disabled="editProfile == false" value="{{ $profile->address}}">
-=======
-                                    <input type="text" name="address" class="form-control" data-ng-disabled="editProfile == false" value="">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                         </div>
@@ -91,33 +71,19 @@
                             <div class="col-md-4">
                                 <div class="form-group label-floating">
                                     <label class="control-label">City</label>
-<<<<<<< HEAD
                                     <input type="text" name="city" class="form-control" data-ng-disabled="editProfile == false" value="{{ $profile->city}}">
-=======
-                                    <input type="text" name="city" class="form-control" data-ng-disabled="editProfile == false" value="">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                             <div class=" col-md-4 ">
                                 <div class="form-group label-floating ">
                                     <label class="control-label ">Country</label>
-<<<<<<< HEAD
                                     <input type="text " name="country " class="form-control " data-ng-disabled="editProfile==false " value="{{ $profile->country}}">
-=======
-                                    <input type="text " name="country " class="form-control " data-ng-disabled="editProfile==f
-                                        alse " value=" ">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                             <div class="col-md-4 ">
                                 <div class="form-group label-floating ">
                                     <label class="control-label ">Postal Code</label>
-<<<<<<< HEAD
                                     <input type="text " name="codePostal " class="form-control " data-ng-disabled="editProfile==false" value="{{ $profile->code_postal}}">
-=======
-                                    <input type="text " name="codePostal " class="form-control " data-ng-disabled="editProfile==f
-                                        alse " value=" ">
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                 </div>
                             </div>
                         </div>
@@ -127,21 +93,12 @@
                                     <label>About Me</label>
                                     <div class="form-group label-floating ">
                                         <label class="control-label "></label>
-<<<<<<< HEAD
                                         <textarea class="form-control " name="bio " rows="5 " data-ng-disabled="editProfile==false">{{ $profile->bio}}</textarea>
-=======
-                                        <textarea class="form-control " name="bio " rows="5 " data-ng-disabled="editProfile==f
-                                        alse "> </textarea>
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                                     </div>
                                 </div>
                             </div>
                         </div>
-<<<<<<< HEAD
                         <button data-ng-show="editProfile==true " type="submit " class="btn btn-primary pull-right ">Update Profile</button>
-=======
-                        <button data-ng-show="editProfile==t rue " type="submit " class="btn btn-primary pull-right ">Update Profile</button>
->>>>>>> 3c33715816872a5b00c63c598a669a4d2a3c9ea0
                         <div class="clearfix "></div>
                     </form>
                 </div>
