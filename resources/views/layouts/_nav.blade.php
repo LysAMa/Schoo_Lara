17d2afc0fd429c9c0565adb018efa71024bb6455
Ray100 <jraymonddoirin@gmail.com>
2018-03-06 11:44:23 -0500

    se connecter en deconexion

--- resources/views/layouts/_nav.blade.php
+++ resources/views/layouts/_nav.blade.php
@@ -1,13 +1,13 @@
 <header class="blog-header py-3">
   <div class="row flex-nowrap justify-content-between align-items-center">
     <div class="col-4 pt-1">
-      <a class="text-muted" href="{{route('register')}}">S'inscrire</a>
+      <a class="text-muted" href="{{route('register')}}">{{Auth::user() ? '' : 'S\'inscrire'}}</a>
     </div>
     <div class="col-4 text-center">
       <a class="blog-header-logo text-dark" href="{{ route('index') }}">Schoda</a>
     </div>
     <div class="col-4 d-flex justify-content-end align-items-center">
-      <a class="btn btn-sm btn-outline-secondary" href="{{route('login')}}"> Se connecter</a>
+      <a class="btn btn-sm btn-outline-secondary" href="{{route('login')}}">{{Auth::user() ? 'Deconexion' : 'Se connecter'}}</a>
     </div>
   </div>
 </header>
