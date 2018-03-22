dfe8b69df41b21aa3fa6cc6dd956f17374c43b58
edFortune <fortune.edmael1@gmail.com>
2018-03-22 10:38:29 -0400

    Add Post view to home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,46 +1,207 @@
-@extends('layouts/master')
+@extends('layouts/master') @section('content') @include('layouts/_nav')
 
+<!--
+<div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
+  <div class="col-md-6 px-0">
+    <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
+    <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting
+      in this post's contents.</p>
+    <p class="lead mb-0">
+      <a href="#" class="text-white font-weight-bold">Continue reading...</a>
+    </p>
+  </div>
+</div>
+-->
+<hr>
+<div class="row mb-2">
+  <div class="col-md-12">
 
-@section('content')
+    <div class="ui cards">
 
-    @include('layouts/_nav')
-
-    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-      <div class="col-md-6 px-0">
-        <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-        <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
-      </div>
-    </div>
-
-    <div class="row mb-2">
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <strong class="d-inline-block mb-2 text-primary">Org Name</strong>
-            <h3 class="mb-0">
-              <a class="text-dark" href="#">Featured post</a>
-            </h3>
-            <div class="mb-1 text-muted">Nov 12</div>
-            <p class="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique odit, cupiditate nobis fugiat nostrum sed libero, mollitia hic facilis saepe.</p>
-            <a href="#">Continue reading</a>
+      <div class="card">
+        <div class="content">
+          <img class="right floated ui avatar image" src="{{asset('images/steve.jpg')}}">
+          <div class="header">
+            Elliot Fu
           </div>
-          <img class="card-img-right flex-auto d-none d-md-block" src="{{ asset('images/img.png')}}" alt="Card image cap">
+          <div class="meta">
+            Friends of Veronika
+          </div>
+          <div class="description">
+            <img class="img-fluid rounded" src="{{asset('images/350x150.png')}}"> Elliot requested permission to view your contact details
+          </div>
+        </div>
+
+        <div class="extra content">
+
+          <span class="right floated">
+            <i class="heart outline like icon"></i>
+            17 likes
+          </span>
+          <i class="comment icon"></i>
+          2 comments
+
+        </div>
+
+        <div class="extra content">
+          <div>Comments</div>
+          <div class="ui comments">
+            <div class="comment">
+              <a class="avatar">
+                <img src="{{asset('images/joe.jpg')}}">
+              </a>
+              <div class="content">
+                <a class="author">Tom Lukic</a>
+                <div class="metadata">
+                  <div class="date">2 days ago</div>
+                  <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                  </div>
+                </div>
+                <div class="text">
+                  This will be great for business reports. I will definitely download this.
+                </div>
+                <div class="actions">
+                  <a class="reply">Reply</a>
+                  <a class="save">Save</a>
+                  <a class="hide">Hide</a>
+                  <a>
+                    <i class="expand icon"></i>
+                    Full-screen
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 box-shadow h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <strong class="d-inline-block mb-2 text-success">Org Name</strong>
-            <h3 class="mb-0">
-              <a class="text-dark" href="#">Post title</a>
-            </h3>
-            <div class="mb-1 text-muted">Nov 11</div>
-            <p class="card-text mb-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni voluptatem consequatur dolorem. Libero id eos, qui voluptates ullam, incidunt est?</p>
-            <a href="#">Continue reading</a>
+
+      <div class="card">
+        <div class="content">
+          <img class="right floated ui avatar image" src="{{asset('images/steve.jpg')}}">
+          <div class="header">
+            Elliot Fu
           </div>
-          <img class="card-img-right flex-auto d-none d-md-block" src="{{ asset('images/img.png')}}" alt="Card image cap">
+          <div class="meta">
+            Friends of Veronika
+          </div>
+          <div class="description">
+            <img class="img-fluid rounded" src="{{asset('images/350x150.png')}}"> Elliot requested permission to view your contact details
+          </div>
+        </div>
+
+        <div class="extra content">
+
+          <span class="right floated">
+            <i class="heart outline like icon"></i>
+            17 likes
+          </span>
+          <i class="comment icon"></i>
+          2 comments
+
+        </div>
+
+        <div class="extra content">
+          <div>Comments</div>
+          <div class="ui comments">
+            <div class="comment">
+              <a class="avatar">
+                <img src="{{asset('images/joe.jpg')}}">
+              </a>
+              <div class="content">
+                <a class="author">Tom Lukic</a>
+                <div class="metadata">
+                  <div class="date">2 days ago</div>
+                  <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                  </div>
+                </div>
+                <div class="text">
+                  This will be great for business reports. I will definitely download this.
+                </div>
+                <div class="actions">
+                  <a class="reply">Reply</a>
+                  <a class="save">Save</a>
+                  <a class="hide">Hide</a>
+                  <a>
+                    <i class="expand icon"></i>
+                    Full-screen
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <div class="card">
+        <div class="content">
+          <img class="right floated ui avatar image" src="{{asset('images/steve.jpg')}}">
+          <div class="header">
+            Elliot Fu
+          </div>
+          <div class="meta">
+            Friends of Veronika
+          </div>
+          <div class="description">
+            <img class="img-fluid rounded" src="{{asset('images/350x150.png')}}"> Elliot requested permission to view your contact details
+          </div>
+        </div>
+
+        <div class="extra content">
+
+          <span class="right floated">
+            <i class="heart outline like icon"></i>
+            17 likes
+          </span>
+          <i class="comment icon"></i>
+          2 comments
+
+        </div>
+
+        <div class="extra content">
+          <div>Comments</div>
+          <div class="ui comments">
+            <div class="comment">
+              <a class="avatar">
+                <img src="{{asset('images/joe.jpg')}}">
+              </a>
+              <div class="content">
+                <a class="author">Tom Lukic</a>
+                <div class="metadata">
+                  <div class="date">2 days ago</div>
+                  <div class="rating">
+                    <i class="star icon"></i>
+                    5 Faves
+                  </div>
+                </div>
+                <div class="text">
+                  This will be great for business reports. I will definitely download this.
+                </div>
+                <div class="actions">
+                  <a class="reply">Reply</a>
+                  <a class="save">Save</a>
+                  <a class="hide">Hide</a>
+                  <a>
+                    <i class="expand icon"></i>
+                    Full-screen
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
     </div>
+
+
+  </div>
+
+</div>
 @stop
